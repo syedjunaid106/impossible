d89cb5f34faff488671d4517cc5b678a9842f01f
syedjunaid106 <76994131+syedjunaid106@users.noreply.github.com>
2024-03-25 11:42:45 +0500

    initail elements before header components

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,12 @@
 import { Button } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
   import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs";
-import { useMutation } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import Image from "next/image";
 
 export default function Home() {
-  const createfile=useMutation(api.files.createfile)
+  const createfile=useMutation(api.files.createFile)
+  const files=useQuery(api.files.getFiles);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <SignedIn>
@@ -19,9 +20,12 @@ export default function Home() {
         <Button>Signin</Button>
       </SignInButton>
     </SignedOut>
+    {files?.map((file)=>{
+      return <div key={file._id}>{file.name}</div>
+    })}
     <button onClick={()=>{
       createfile({
-        name:"shah"
+        name:"juni"
       })
     }}>Click me</button>
     </main>
--- convex/files.ts
+++ convex/files.ts
@@ -1,12 +1,27 @@
-import { v } from 'convex/values';
-import {mutation} from './_generated/server';
-export const createfile=mutation({
+import { ConvexError, v } from 'convex/values';
+import {mutation, query} from './_generated/server';
+export const createFile=mutation({
     args:{
         name:v.string()
     },
    async handler(ctx,args){
+    const identify= await ctx.auth.getUserIdentity();
+    if(!identify){
+        throw new ConvexError('you must be logged in to upload a file')
+    }
     await ctx.db.insert('files',{
         name:args.name
     })
    }
+})
+export const  getFiles=query({
+    
+    args:{
+    },
+   async handler(ctx,args){
+    const identify= await ctx.auth.getUserIdentity();
+    if(!identify){
+        return [];
+    }
+    return ctx.db.query('files').collect()}
 })
